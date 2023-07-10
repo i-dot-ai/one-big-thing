be0d35a1835eabb657244e68104d7814988b8616
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-07-10 18:00:35 +0100

    Added function to interface for creating a completion of a course

--- one_big_thing/learning/interface.py
+++ one_big_thing/learning/interface.py
@@ -1,11 +1,21 @@
 import marshmallow
 
 from . import models, schemas
-from .utils import Entity, Facade, register_event, with_schema
+from .utils import Entity, register_event, with_schema, Interface
 
 
 class CreateCourseSchema(marshmallow.Schema):
     data = marshmallow.fields.Nested(schemas.CourseSchema)
+
+
+class CreateCompletionSchema(marshmallow.Schema):
+    user_id = marshmallow.fields.UUID()
+    course_id = marshmallow.fields.UUID()
+    user_to_add = marshmallow.fields.UUID()
+
+
+class CreateCompletionResponseSchema(marshmallow.Schema):
+    completion_id = marshmallow.fields.UUID()
 
 
 class Course(Entity):
@@ -19,4 +29,18 @@ class Course(Entity):
         return course
 
 
-facade = Facade(course=Course())
+class Completion(Entity):
+    @with_schema(load=CreateCompletionSchema, dump=CreateCompletionResponseSchema)
+    @register_event("Completion created")
+    def create(self, user_id, course_id, user_to_add):
+        completion = models.Completion()
+        user = models.User.objects.get(pk=user_to_add)
+        course = models.Course.objects.get(pk=course_id)
+        completion.course = course
+        completion.user = user
+        completion.save()
+        response = {"completion_id": completion.id}
+        return response
+
+
+api = Interface(course=Course(), completion=Completion())
