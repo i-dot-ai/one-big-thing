ac81e28dabb1b2f0bcd46306e87e5d8469985e8b
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-07-10 17:19:28 +0100

    Changed the import method for test utils

--- tests/utils.py
+++ tests/utils.py
@@ -3,7 +3,7 @@ import functools
 import httpx
 import testino
 
-import one_big_thing.wsgi
+from one_big_thing import wsgi
 
 TEST_SERVER_URL = "http://one_big_thing-testserver:8010/"
 
@@ -11,12 +11,12 @@ TEST_SERVER_URL = "http://one_big_thing-testserver:8010/"
 def with_client(func):
     @functools.wraps(func)
     def _inner(*args, **kwargs):
-        with httpx.Client(app=one_big_thing.wsgi.application, base_url=TEST_SERVER_URL) as client:
+        with httpx.Client(app=wsgi.application, base_url=TEST_SERVER_URL) as client:
             return func(client, *args, **kwargs)
 
     return _inner
 
 
 def make_testino_client():
-    client = testino.WSGIAgent(one_big_thing.wsgi.application, TEST_SERVER_URL)
+    client = testino.WSGIAgent(wsgi.application, TEST_SERVER_URL)
     return client
