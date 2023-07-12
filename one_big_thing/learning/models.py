f93abca4ad51c7d8f9283747c2eee2874faa26c4
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-07-12 13:12:14 +0100

    Re-added required fields to user model

--- one_big_thing/learning/models.py
+++ one_big_thing/learning/models.py
@@ -31,6 +31,7 @@ class User(BaseUser, UUIDPrimaryKeyBase):
     verified = models.BooleanField(default=False, blank=True, null=True)
     invited_at = models.DateTimeField(default=None, blank=True, null=True)
     invite_accepted_at = models.DateTimeField(default=None, blank=True, null=True)
+    last_token_sent_at = models.DateTimeField(editable=False, blank=True, null=True)
     is_external_user = models.BooleanField(editable=True, default=False)
 
     def save(self, *args, **kwargs):
