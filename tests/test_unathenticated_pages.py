711396f1c80820062d1697bd15944705fffd8f28
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-08-02 10:29:11 +0100

    remove unnecesary comments

--- tests/test_unathenticated_pages.py
+++ tests/test_unathenticated_pages.py
@@ -1,6 +1,6 @@
 from . import utils
 
-OTHER_URLS_LOGIN_NOT_REQUIRED = ["/"]
+
 ACCOUNT_URLS_LOGIN_NOT_REQUIRED = [
     "/accounts/verify/",
     "/accounts/password-reset/",
@@ -11,9 +11,6 @@ ACCOUNT_URLS_LOGIN_NOT_REQUIRED = [
     "/accounts/signup/",
     "/accounts/verify/resend/",
 ]
-
-# TODO - all auth urls?
-# TODO - should we need login for some of these views?
 
 
 @utils.with_client
