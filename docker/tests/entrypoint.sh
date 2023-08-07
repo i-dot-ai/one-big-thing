961515a6ee97b8abae06764137a6053aa2a54e24
252afh <elliotmoorecornwall@outlook.com>
2023-08-07 14:48:18 +0100

    Removed database url from echoing in tests startup

--- docker/tests/entrypoint.sh
+++ docker/tests/entrypoint.sh
@@ -6,8 +6,6 @@ set -o nounset
 DATABASE_URL_VARIABLE_NAME="${ENVIRONMENT}_DATABASE_URL"
 DATABASE_URL_VALUE=$(eval "echo \$$DATABASE_URL_VARIABLE_NAME")
 
-echo $DATABASE_URL_VALUE
-
 wait-for-db --mode postgres --connection-string $DATABASE_URL_VALUE --timeout 60 --sql-query "select 1;"
 
 exec "$@"
