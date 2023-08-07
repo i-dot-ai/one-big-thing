9c1f4ff4fbe09d77ac6359035fd6e04f197d19d1
252afh <elliotmoorecornwall@outlook.com>
2023-08-07 14:50:16 +0100

    Re-added wait for db to web startup

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -3,4 +3,9 @@
 set -o errexit
 set -o nounset
 
+DATABASE_URL_VARIABLE_NAME="${ENVIRONMENT}_DATABASE_URL"
+DATABASE_URL_VALUE=$(eval "echo \$$DATABASE_URL_VARIABLE_NAME")
+
+wait-for-db --mode postgres --connection-string $DATABASE_URL_VALUE --timeout 60 --sql-query "select 1;"
+
 exec "$@"
