83ab7cf6bc3cb9e0693e068a7a4cbaa8b0ebbdc9
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-08-08 15:45:46 +0100

    skeleton for check delete learning page

--- one_big_thing/learning/views.py
+++ one_big_thing/learning/views.py
@@ -463,11 +463,27 @@ def send_learning_record_view(request):
 
 
 @login_required
+@require_http_methods(["GET", "POST"])
+@enforce_user_completes_pre_survey
+def check_delete_learning_view(request, learning_id):
+    if request.POST:
+        # TODO
+        # Delete 
+        # or cancel
+        pass
+    return render(request, "delete-learning-check.html")
+
+
+
+@login_required
 @require_http_methods(["POST"])
 @enforce_user_completes_pre_survey
-def remove_learning_view(request, learning_id):
+def delete_learning_view(request, learning_id):
     interface.api.learning.delete(user_id=request.user.id, learning_id=learning_id)
     return redirect("record-learning")
+
+
+
 
 
 @login_required
