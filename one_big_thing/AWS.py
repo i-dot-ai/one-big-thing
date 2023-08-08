a9b84d27f88c73f57f0df949f654734140cd130c
252afh <elliotmoorecornwall@outlook.com>
2023-08-08 14:24:02 +0100

    Removed env from variable name and added it to secrets call for AWS

--- one_big_thing/AWS.py
+++ one_big_thing/AWS.py
@@ -5,10 +5,12 @@ import boto3
 
 AWS_REGION = "eu-west-2"
 
+app_name = "one-big-thing"
+
 
 def get_secret(name: str):
     secrets_manager = boto3.client(service_name="secretsmanager", region_name=AWS_REGION)
-    reponse = secrets_manager.get_secret_value(SecretId=name)
+    reponse = secrets_manager.get_secret_value(SecretId=app_name)
 
     if "SecretString" in reponse:
         return reponse["SecretString"]
@@ -16,9 +18,5 @@ def get_secret(name: str):
         return base64.b64decode(reponse["SecretBinary"])
 
 
-def fetch_db_password(secret_name):
-    return json.loads(get_secret(secret_name))["password"]
-
-
-def fetch_generic_secret(secret_name):
-    return json.loads(get_secret(secret_name))[secret_name]
+def fetch_generic_secret(env, secret_name):
+    return json.loads(get_secret(secret_name))[env][secret_name]
