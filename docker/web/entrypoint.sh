c8d99d50217951a39c130cce68d3a6de764d7e02
Ed Singleton <singletoned@gmail.com>
2023-08-18 15:30:44 +0100

    Check DB_URL exists before waiting

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o nounset
 
-if [ "$DEBUG" = true ]
+if [ "$DEBUG" = true ] && [ -z "$DATABASE_URL" ]
 then
   wait-for-db --mode postgres --connection-string "$DATABASE_URL" --timeout 60 --sql-query "select 1;"
 fi
