0931531cc7760ac4114ff83f15536d1213819363
Ed Singleton <singletoned@gmail.com>
2023-08-23 14:44:14 +0100

    Refactor auth views

--- one_big_thing/learning/authentication_views.py
+++ one_big_thing/learning/authentication_views.py
@@ -38,7 +38,7 @@ def _strip_microseconds(dt):
 
 @require_http_methods(["GET", "POST"])
 class CustomLoginView(MethodDispatcher):
-    template_name = "account/login.html"
+    template_name = "login.html"
     error_message = "Something has gone wrong.  Please try again."
 
     def error(self, request):
@@ -46,154 +46,38 @@ class CustomLoginView(MethodDispatcher):
         return render(request, self.template_name)
 
     def get(self, request):
-        return render(request, self.template_name)
+        context = {"errors": {}}
+        return render(request, self.template_name, context)
 
     def post(self, request):
-        password = request.POST.get("password", None)
         email = request.POST.get("login", None)
-        if not password or not email:
-            messages.error(request, "Please enter an email and password.")
+        if not email:
+            messages.error(request, "Please enter an email.")
             return render(request, self.template_name, {})
         else:
             email = email.lower()
-            user = authenticate(request, email=email, password=password)
+            user = models.User.objects.get(email=email)
             if user is not None:
-                if settings.SEND_VERIFICATION_EMAIL:
-                    if not user.verified:
-                        return render(
-                            request, "account/login.html", {"resend_verification": True, "resend_email": user.email}
-                        )
-                login(request, user)
-                request.session["session_created_at"] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-                return redirect(reverse("homepage"))
-            else:
-                return self.error(request)
-
-
-@require_http_methods(["GET", "POST"])
-class CustomResendVerificationView(MethodDispatcher):
-    def get(self, request):
-        email = request.GET.get("email")
-        if not email:
-            return render(request, "account/resend_verification_email.html", {})
-        else:
-            user = models.User.objects.filter(email=email).first()
-            if not user:
-                return render(request, "account/resend_verification_email.html", {})
-            email_handler.send_verification_email(user)
-            return render(
-                request,
-                "account/signup_complete.html",
-                {},
-            )
-
-    def post(self, request):
-        email = request.POST.get("email")
-        if not email:
-            messages.error(request, "Please enter a valid email address.")
-            return render(request, "account/resend_verification_email.html", {})
-        try:
-            validate_email(email)
-        except ValidationError as exc:
-            for errors in exc.error_list:
-                for error in errors:
-                    messages.error(request, error)
-            return render(request, "account/resend_verification_email.html", {})
-        try:
-            restrict_email.clean_email(email=email)
-        except ValidationError as exc:
-            for errors in exc.error_list:
-                for error in errors:
-                    messages.error(request, error)
-            return render(request, "account/resend_verification_email.html", {})
-        user = models.User.objects.filter(email=email).first()
-        if user:
-            email_handler.send_verification_email(user)
-        return render(request, "account/signup_complete.html", {})
-
-
-class CustomSignupView(SignupView):
-    def get_context_data(self, **kwargs):
-        department_choices = departments.Department.choices
-        context = super(CustomSignupView, self).get_context_data(**kwargs)
-        context["departments"] = department_choices
-        context["grades"] = choices.Grade.choices
-        context["professions"] = choices.Profession.choices
-        return context
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.method == "POST":
-            email = request.POST.get("email")
-            password1 = request.POST.get("password1")
-            password2 = request.POST.get("password2")
-            department = request.POST.get("department")
-            grade = request.POST.get("grade")
-            profession = request.POST.get("profession")
-            context = {
-                "departments": departments.Department.choices,
-                "grades": choices.Grade.choices,
-                "professions": choices.Profession.choices,
-            }
-            try:
-                validate_email(email)
-            except ValidationError as exc:
-                for errors in exc.error_list:
-                    for error in errors:
-                        messages.error(request, error)
-                return render(request, self.template_name, context)
-            try:
-                restrict_email.clean_email(email=email)
-            except ValidationError as exc:
-                for errors in exc.error_list:
-                    for error in errors:
-                        messages.error(request, error)
-                return render(request, self.template_name, context)
-            try:
-                validate_password(password1)
-                # Replicate UserAttributeSimilarityValidator which
-                # isn't used as user not yet created
-                similarity_password_validator(password1, email)
-            except ValidationError as exc:
-                for errors in exc.error_list:
-                    for error in errors:
-                        messages.error(request, error)
-                return render(request, self.template_name, context)
-            if password1 != password2:
-                messages.error(request, "You must type the same password each time.")
-                return render(request, self.template_name, context)
-            existing_user = models.User.objects.filter(email=email)
-            if existing_user.exists():
-                send_account_already_exists_email(existing_user.first())
-                return render(
-                    request,
-                    "account/signup_complete.html",
-                    {},
-                )
-            if not department or not grade or not profession:
-                if not department:
-                    messages.error(request, "You must select a department.")
-                if not grade:
-                    messages.error(request, "You must select a grade.")
-                if not profession:
-                    messages.error(request, "You must select a profession.")
-                return render(request, self.template_name, context)
-            user = models.User.objects.create_user(
-                email=email, password=password1, department=department, grade=grade, profession=profession
-            )
-            user.save()
-            if settings.SEND_VERIFICATION_EMAIL:
                 email_handler.send_verification_email(user)
-                return render(
-                    request,
-                    "account/signup_complete.html",
-                    {},
-                )
-            user = authenticate(request, email=email, password=password1)
-            login(request, user)
-            messages.success(request, f"Successfully signed in as {user.email}.")
-            return redirect(reverse("homepage"))
-        response = super().dispatch(request, errors={}, *args, **kwargs)
-        return response
+            else:
+                try:
+                    validate_email(email)
+                except ValidationError as exc:
+                    for errors in exc.error_list:
+                        for error in errors:
+                            messages.error(request, error)
+                    return render(request, self.template_name)
+                try:
+                    restrict_email.clean_email(email=email)
+                except ValidationError as exc:
+                    for errors in exc.error_list:
+                        for error in errors:
+                            messages.error(request, error)
+                    return render(request, self.template_name)
+
+                user = models.User.objects.create_user(email=email)
+                email_handler.send_register_email(user)
+                return redirect(reverse("email_sent"))
 
 
 @require_http_methods(["GET"])
@@ -210,79 +94,56 @@ class CustomVerifyUserEmail(MethodDispatcher):
             user.save()
             user.backend = "django.contrib.auth.backends.ModelBackend"
             login(request, user)
-            return redirect(reverse("homepage"))
+            return redirect(reverse("register"))
         return render(request, "account/verify_email_from_token.html", {"verify_result": verify_result})
 
 
-@require_http_methods(["GET", "POST"])
-class PasswordChange(MethodDispatcher):
-    password_reset_error_message = (
-        "This link is not valid. It may have expired or have already been used. Please try again."
-    )
+class CustomRegisterView(MethodDispatcher):
+    template_name = "register.html"
+    error_message = "Something has gone wrong.  Please try again."
 
-    def get_token_request_args(self, request):
-        user_id = request.GET.get("user_id", None)
-        token = request.GET.get("code", None)
-        valid_request = False
-        if not user_id or not token:
-            messages.error(request, self.password_reset_error_message)
+    def error(self, request):
+        messages.error(request, self.error_message)
+        return render(request, self.template_name)
+
+    def get(self, request):
+        department_choices = departments.Department.choices
+        context = {
+            "departments": department_choices,
+            "grades": choices.Grade.choices,
+            "professions": choices.Profession.choices,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        user = request.user
+        department = request.POST.get("department")
+        grade = request.POST.get("grade")
+        profession = request.POST.get("profession")
+        department_choices = departments.Department.choices
+        context = {
+            "departments": department_choices,
+            "grades": choices.Grade.choices,
+            "professions": choices.Profession.choices,
+        }
+
+        if not department or not grade or not profession:
+            if not department:
+                messages.error(request, "You must select a department.")
+            if not grade:
+                messages.error(request, "You must select a grade.")
+            if not profession:
+                messages.error(request, "You must select a profession.")
+            return render(request, self.template_name, context)
         else:
-            result = email_handler.verify_token(user_id, token, "password-reset")
-            if not result:
-                messages.error(request, self.password_reset_error_message)
-            else:
-                valid_request = True
-        return user_id, token, valid_request
+            user.department=department
+            user.grade=grade
+            user.profession=profession
+            user.save()
 
-    def get(self, request):
-        try:
-            _, _, valid_request = self.get_token_request_args(request)
-            return render(request, "account/password_reset_from_key.html", {"valid": valid_request})
-        except models.User.DoesNotExist:
-            return render(request, "account/password_reset_from_key.html", {"valid": False})
+            return render(
+                request,
+                "account/signup_complete.html",
+                {},
+            )
 
-    def post(self, request):
-        user_id, token, valid_request = self.get_token_request_args(request)
-        pwd1 = request.POST.get("password1", None)
-        pwd2 = request.POST.get("password2", None)
-        if pwd1 != pwd2:
-            messages.error(request, "Passwords must match.")
-            return render(request, "account/password_reset_from_key.html", {"valid": valid_request})
-        if not valid_request:
-            messages.error(request, self.password_reset_error_message)
-            return render(request, "account/password_reset_from_key.html", {"valid": valid_request})
-        user = models.User.objects.get(pk=user_id)
-        try:
-            validate_password(pwd1, user)
-        except ValidationError as e:
-            for msg in e:
-                messages.error(request, str(msg))
-            return render(request, "account/password_reset_from_key.html", {"valid": valid_request})
-        user.set_password(pwd1)
-        user.save()
-        return render(request, "account/password_reset_from_key_done.html", {})
-
-
-@require_http_methods(["GET", "POST"])
-class PasswordReset(MethodDispatcher):
-    def get(self, request):
-        return render(request, "account/password_reset.html", {})
-
-    def post(self, request):
-        email = request.POST.get("email")
-        try:
-            user = models.User.objects.get(email=email)
-        except models.User.DoesNotExist:
-            return redirect("password-reset-done")
-        email_handler.send_password_reset_email(user)
-        return redirect("password-reset-done")
-
-
-@require_http_methods(["GET"])
-def password_reset_done(request):
-    return render(request, "account/password_reset_done.html", {})
-
-
-@require_http_methods(["GET"])
-def password_reset_from_key_done(request):
-    return render(request, "account/password_reset_from_key_done.html", {})
