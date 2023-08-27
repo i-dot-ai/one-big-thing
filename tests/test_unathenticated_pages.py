e60b6306db18b764f0e5061417dc8d886c6bb36b
Ed Singleton <singletoned@gmail.com>
2023-08-27 16:04:17 +0100

    Refactor more login tests

--- tests/test_unathenticated_pages.py
+++ tests/test_unathenticated_pages.py
@@ -4,7 +4,7 @@ from one_big_thing.learning import models
 
 from . import utils
 
-ACCOUNT_URLS_LOGIN_NOT_REQUIRED = [
+ACCOUNT_URLS_MISSING = [
     "/accounts/verify/",
     "/accounts/password-reset/",
     "/accounts/change-password/reset/",
@@ -19,91 +19,24 @@ INFO_PAGES_LOGIN_NOT_REQUIRED = ["/privacy-notice/", "/support/", "/accessibilit
 
 
 @utils.with_client
-def test_get_account_urls_login_not_required(client):
-    for url in ACCOUNT_URLS_LOGIN_NOT_REQUIRED:
+def test_get_account_urls_not_there(client):
+    for url in ACCOUNT_URLS_MISSING:
         response = client.get(url)
-        assert response.status_code == 200, response.status_code
-
-
-def test_login():
-    client = utils.make_testino_client()
-    email = "fred@example.com"
-    password = "StupidPassword1!"
-    page = client.get("/accounts/signup/")
-    form = page.get_form()
-    form["email"] = email
-    form["password1"] = password
-    form["password2"] = password
-    form["grade"] = "GRADE6"
-    form["department"] = "visitengland"
-    form["profession"] = "LEGAL"
-    form.submit().follow()
-    page = client.get("/").follow()
-    # email verification off for tests
-    assert page.has_text("Thank you for registering for One Big Thing.")
-    page = client.get("/accounts/login/")
-    form = page.get_form()
-    form["login"] = email
-    form["password"] = "wrong_password"
-    page = form.submit()
-    assert page.has_text("Something has gone wrong.")
-    form = page.get_form()
-    form["login"] = email
-    form["password"] = password
-    form.submit()
-    page = client.get("/").follow()
-    assert page.has_text("Thank you for registering for One Big Thing.")
-    models.User.objects.get(email=email).delete()
+        assert response.status_code == 404, response.status_code
 
 
 @override_settings(SEND_VERIFICATION_EMAIL=True)
-def test_email_verification():
+def test_login():
     client = utils.make_testino_client()
     email = "james@example.com"
-    password = "StupidPassword1!"
-    page = client.get("/accounts/signup/")
+    page = client.get("/")
     form = page.get_form()
     form["email"] = email
-    form["password1"] = password
-    form["password2"] = password
-    form["grade"] = "GRADE7"
-    form["department"] = "hm-treasury"
-    form["profession"] = "LEGAL"
-    page = form.submit()
-    assert page.has_text("Sign up complete")
-    assert page.has_text("verification email")
+    page = form.submit().follow()
+    assert page.has_text("Sign in email sent")
     verification_url = utils._get_latest_email_url()
     # Verification should take to homepage, which redirects to start of survey
     page = client.get(verification_url).follow()
     assert page.status_code == 302, page.status_code  # Homepage redirects
     page = page.follow()
     assert page.has_text("Thank you for registering for One Big Thing")
-
-
-def test_invalid_password_requirements():
-    email = "Annie4@example.com"
-    invalid_passwords = [
-        "password1!",
-        "elephants$5",
-        "PinkPanda",
-        "h12!L",
-        email,
-        "AnniE4@",
-        "70Whitehall!",
-        "CabinetOffice4eva!",
-        "10-Downing-Street",
-        "One_Big_Thing_23",
-    ]
-    client = utils.make_testino_client()
-    for pwd in invalid_passwords:
-        page = client.get("/accounts/signup/")
-        form = page.get_form()
-        form["email"] = email
-        form["grade"] = "GRADE7"
-        form["department"] = "home-office"
-        form["profession"] = "ANALYSIS"
-        form["password1"] = pwd
-        form["password2"] = pwd
-        page = form.submit()
-        # ie return to registration page unable to progress
-        assert page.has_text("Register"), pwd
