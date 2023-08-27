f6d216929f2ee06de3f078714b12cb40e844db6d
Ed Singleton <singletoned@gmail.com>
2023-08-27 13:32:19 +0100

    Refactor _send_token_email

--- one_big_thing/learning/email_handler.py
+++ one_big_thing/learning/email_handler.py
@@ -36,77 +36,80 @@ PASSWORD_RESET_TOKEN_GENERATOR = PasswordResetTokenGenerator()
 
 EMAIL_MAPPING = {
     "email-verification": {
-        "from_address": settings.FROM_EMAIL,
         "subject": "One Big Thing: confirm your email address",
         "template_name": "email/verification.txt",
         "url_name": "verify-email",
         "token_generator": EMAIL_VERIFY_TOKEN_GENERATOR,
     },
     "password-reset": {
-        "from_address": settings.FROM_EMAIL,
         "subject": "One Big Thing: password reset",
         "template_name": "email/password-reset.txt",
         "url_name": "password-set",
         "token_generator": PASSWORD_RESET_TOKEN_GENERATOR,
     },
     "account-already-exists": {
-        "from_address": settings.FROM_EMAIL,
         "subject": "One Big Thing: registration attempt",
         "template_name": "email/account-already-exists.txt",
         "url_name": "password-reset",
     },
     "send-learning-record": {
-        "from_address": settings.FROM_EMAIL,
         "subject": "One Big Thing: Your learning record",
         "template_name": "email/send-learning-record.txt",
     },
 }
 
 
-def _send_token_email(user, subject, template_name, from_address, url_name, token_generator):
+def _make_token_url(user, token_type):
+    token_generator = EMAIL_MAPPING[token_type]["token_generator"]
     user.last_token_sent_at = timezone.now()
     user.save()
     token = token_generator.make_token(user)
     base_url = settings.BASE_URL
-    url_path = reverse(url_name)
+    url_path = reverse(EMAIL_MAPPING[token_type]["url_name"])
     url = str(furl.furl(url=base_url, path=url_path, query_params={"code": token, "user_id": str(user.id)}))
+    return url
+
+
+def _send_token_email(user, token_type):
+    url = _make_token_url(user, token_type)
     context = dict(user=user, url=url, contact_address=settings.CONTACT_EMAIL)
-    body = render_to_string(template_name, context)
+    body = render_to_string(EMAIL_MAPPING[token_type]["template_name"], context)
     response = send_mail(
-        subject=subject,
+        subject=EMAIL_MAPPING[token_type]["subject"],
         message=body,
-        from_email=from_address,
+        from_email=settings.FROM_EMAIL,
         recipient_list=[user.email],
     )
     return response
 
 
-def _send_normal_email(subject, template_name, from_address, to_address, context):
+def _send_normal_email(subject, template_name, to_address, context):
     body = render_to_string(template_name, context)
     response = send_mail(
         subject=subject,
         message=body,
-        from_email=from_address,
+        from_email=settings.FROM_EMAIL,
         recipient_list=[to_address],
     )
     return response
 
 
 def send_password_reset_email(user):
-    data = EMAIL_MAPPING["password-reset"]
-    return _send_token_email(user, **data)
+    return _send_token_email(user, "password-reset")
 
 
 def send_invite_email(user):
-    data = EMAIL_MAPPING["invite-user"]
     user.invited_at = timezone.now()
     user.save()
-    return _send_token_email(user, **data)
+    return _send_token_email(user, "invite-user")
 
 
 def send_verification_email(user):
-    data = EMAIL_MAPPING["email-verification"]
-    return _send_token_email(user, **data)
+    return _send_token_email(user, "email-verification")
+
+
+def send_register_email(user):
+    return _send_token_email(user, "email-register")
 
 
 def send_account_already_exists_email(user):
@@ -119,7 +122,6 @@ def send_account_already_exists_email(user):
     response = _send_normal_email(
         subject=data["subject"],
         template_name=data["template_name"],
-        from_address=data["from_address"],
         to_address=user.email,
         context=context,
     )
@@ -132,7 +134,6 @@ def send_learning_record_email(user, email):
     response = _send_normal_email(
         subject=data["subject"],
         template_name=data["template_name"],
-        from_address=data["from_address"],
         to_address=email,
         context=context,
     )
