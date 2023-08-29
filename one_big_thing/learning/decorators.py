5f459760c2006009c547a669215b62ee30c2f894
Ed Singleton <singletoned@gmail.com>
2023-08-29 22:21:58 +0100

    Add login_required decorator

--- one_big_thing/learning/decorators.py
+++ one_big_thing/learning/decorators.py
@@ -6,6 +6,15 @@ def unauthorised_view(request):
     return render(request, "unauthorised.html", {}, status=401)
 
 
+def login_required(func):
+    def _inner(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return func(request, *args, **kwargs)
+        else:
+            return unauthorised_view(request)
+    return _inner
+
+
 enforce_user_completes_pre_survey = user_passes_test(
     lambda user: (user.has_completed_pre_survey and user.is_active),
     login_url="/intro-pre-survey/",
