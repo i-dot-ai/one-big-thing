ae11053efbc0f836d3fdf56a36c447c598d102a3
Ed Singleton <singletoned@gmail.com>
2023-08-30 08:54:57 +0100

    Refactor reload script for clarity

--- static/reload.js
+++ static/reload.js
@@ -1,3 +1,5 @@
+const loggedInString = " logged in at ";
+
 fetch("/home/")
   .then(r => r.text())
-  .then(t => {if (t.includes("Sign out")){location.href="/home/"}}) ;
+  .then(t => {if (t.includes(loggedInString)){location.href="/home/"}}) ;
