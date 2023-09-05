f63c129ac67f8a891b822c66757bf2114adfa06c
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-09-05 13:52:34 +0100

    add to admin page

--- one_big_thing/learning/admin.py
+++ one_big_thing/learning/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from . import models
 
+
+class SurveyResultAdmin(admin.ModelAdmin):
+    readonly_fields = ("id", "modified_at", "created_at")
+
+
 admin.site.register(models.Course)
 admin.site.register(models.Learning)
-admin.site.register(models.SurveyResult)
+admin.site.register(models.SurveyResult, SurveyResultAdmin)
 admin.site.register(models.User)
