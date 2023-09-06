888520533ea27280f7096c6afd06cc7afa07aa1a
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-09-06 15:07:34 +0100

    add schema

--- one_big_thing/learning/schemas.py
+++ one_big_thing/learning/schemas.py
@@ -64,12 +64,25 @@ class CourseSchema(TimeStampedModelSchema, UUIDPrimaryKeyBaseModelSchema):
     title = SingleLineStr(required=True, validate=validate.Length(max=200))
     link = SingleLineStr(validate=validate.Length(max=256), allow_none=True)
     learning_type = make_choice_field(max_len=256, values=choices.CourseType.values, allow_none=True)
-    time_to_complete = fields.Str(required=False, validate=validate_time_to_complete)
+    time_to_complete = fields.Str(required=False, validate=validate_time_to_complete) # TODO - just use Integer?
 
 
 class LearningSchema(TimeStampedModelSchema, UUIDPrimaryKeyBaseModelSchema):
     title = SingleLineStr(required=True, validate=validate.Length(max=200))
     link = SingleLineStr(validate=validate.Length(max=256), allow_none=True)
     learning_type = make_choice_field(max_len=256, values=choices.CourseType.values, allow_none=True)
-    time_to_complete = fields.Str(required=False, validate=validate_time_to_complete)
+    time_to_complete = fields.Str(required=False, validate=validate_time_to_complete) # TODO - just use Integer?
+    rating = fields.Str(required=False, allow_none=True)
+
+
+
+HOURS_ERROR = "Please enter the hours this course took to complete, for example, 2"
+MINUTES_ERROR = "Please enter the minutes this course took to complete, between 1 and 59"
+
+class RecordLearningSchema():
+    title = SingleLineStr(required=True, validate=validate.Length(max=200))
+    link = SingleLineStr(validate=validate.Length(max=256), allow_none=True)
+    learning_type = make_choice_field(max_len=256, values=choices.CourseType.values, allow_none=True)
+    time_to_complete_hours = fields.Integer(allow_none=True, validate=validate.Range(min=0), error_messages=HOURS_ERROR)
+    time_to_complete_minutes = fields.Integer(validate=validate.Range(min=0, max=59), error_messages=HOURS_ERROR)
     rating = fields.Str(required=False, allow_none=True)
