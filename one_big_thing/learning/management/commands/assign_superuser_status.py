9e61b6ca8809f9157874fa41b6d7bf4c45b883ee
George Burton <g.e.c.burton@gmail.com>
2023-09-13 13:31:35 +0100

    updated test

--- one_big_thing/learning/management/commands/assign_superuser_status.py
+++ one_big_thing/learning/management/commands/assign_superuser_status.py
@@ -1,5 +1,3 @@
-import getpass
-
 from django.core.management import BaseCommand
 from django_otp.plugins.otp_totp.models import TOTPDevice
 
@@ -7,18 +5,20 @@ from one_big_thing.learning.models import User
 
 
 class Command(BaseCommand):
-    help = """This should be run once per environment to set the initial superuser. 
-    Thereafter the superuser should assign new staff users via the admin and send 
+    help = """This should be run once per environment to set the initial superuser.
+    Thereafter the superuser should assign new staff users via the admin and send
     them the link to the Authenticator.
-    
-    Once run this command will return the link to a Time-One-Time-Pass that the 
+
+    Once run this command will return the link to a Time-One-Time-Pass that the
     superuser should use to enable login to the admin portal."""
 
     def add_arguments(self, parser):
         parser.add_argument("-e", "--email", type=str, help="user's email", required=True)
+        parser.add_argument("-p", "--password", type=str, help="user's new password")
 
     def handle(self, *args, **kwargs):
         email = kwargs["email"]
+        password = kwargs["password"]
 
         try:
             user = User.objects.get(email=email)
@@ -30,9 +30,11 @@ class Command(BaseCommand):
             return
 
         user.is_superuser = True
-        if not user.password:
-            password = getpass.getpass("Password: ")
+        if password:
             user.set_password(password)
+        elif not user.password:
+            self.stderr.write(self.style.ERROR(f"A password must be set for '{email}'."))
+            return
 
         user.save()
 
--- tests/test_assign_superuser_status.py
+++ tests/test_assign_superuser_status.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from io import StringIO
 
 from django.core.management import call_command
@@ -8,15 +7,28 @@ from one_big_thing.learning.models import User
 
 def test_assign_superuser_status_happy_path():
     """we create a new user, initially they are not a superuser,
-    we then make them one and check that a OTP link is returned
+    we then make them one and check that:
+    1. a TOTP link is returned
+    2. the user now has a password
+    3. the user is now a superuser
     """
-    email = "diana@co.gov.uk"
+    email, password = "diana@co.gov.uk", "P4ssw0rd!"
     user = User.objects.create_user(email="diana@co.gov.uk")
     assert user.is_superuser is False
 
     stdout = StringIO()
-    call_command("assign_superuser_status", "--email", email, stdout=stdout)
+
+    call_command("assign_superuser_status", "--email", email, "--password", password, stdout=stdout)
+
     lines = stdout.getvalue().split("\n")
+    # 1. do we get the totp link?
     assert lines[0].startswith("otpauth://totp/OneBigThing")
+
+    # fetch updated user state
     user.refresh_from_db()
+
+    # 2. has the password been set correctly?
+    assert user.check_password(password), user.password
+
+    # 3. is the user a superuser?
     assert user.is_superuser is True
