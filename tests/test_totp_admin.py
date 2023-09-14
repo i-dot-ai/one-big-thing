fa6f23caf50b1e767a3d9532607ab66db7fab043
George Burton <g.e.c.burton@gmail.com>
2023-09-14 12:40:37 +0100

    test fixed

--- tests/test_totp_admin.py
+++ tests/test_totp_admin.py
@@ -1,7 +1,8 @@
-from django.test import TestCase, Client
+from django.test import Client, TestCase
 from django_otp.oath import totp
-from one_big_thing.learning.models import User
 from django_otp.plugins.otp_totp.models import TOTPDevice
+
+from one_big_thing.learning.models import User
 
 
 class TOTPAdminTest(TestCase):
@@ -9,10 +10,7 @@ class TOTPAdminTest(TestCase):
         """
         Create a device at the fourth time step. The current token is 154567.
         """
-        self.superuser = User.objects.create_superuser(
-            password='password',
-            email='admin@example.com'
-        )
+        self.superuser = User.objects.create_superuser(password="password", email="admin@example.com")
 
         # Create a TOTP device for the superuser
         self.device = TOTPDevice.objects.create(user=self.superuser, confirmed=True)
@@ -20,34 +18,35 @@ class TOTPAdminTest(TestCase):
         # Create an instance of the Django test client
         self.client = Client()
 
-
     def test_admin_login_success(self):
         # Perform a login POST request to the admin login page
         response = self.client.post(
-            '/admin/login/',
+            "/admin/login/",
             {
-                'username': 'admin@example.com',
-                'password': 'password',
+                "username": "admin@example.com",
+                "password": "password",
                 "otp_token": totp(self.device.bin_key),
-             })
+                "next": "/admin/",
+            },
+        )
 
         # Check if the login was successful (HTTP 302 status code indicates a redirect)
-        self.assertEqual(response.status_code, 200)
-        # Check if the user is redirected to the admin dashboard (change this URL as needed)
-#        self.assertRedirects(response.read(), '/admin/')
+        self.assertEqual(response.status_code, 302)
+        # Check if the user is redirected to the admin dashboard
+        self.assertRedirects(response, "/admin/")
 
     def test_admin_login_fail(self):
         # Perform a login POST request to the admin login page
         response = self.client.post(
-            '/admin/login/',
+            "/admin/login/",
             {
-                'username': 'admin@example.com',
-                'password': 'password',
+                "username": "admin@example.com",
+                "password": "password",
                 "otp_token": 123456,
-             })
+            },
+        )
 
-        # Check if the login was successful (HTTP 302 status code indicates a redirect)
+        # Check if the login was unsuccessful (bizarrely this is an HTTP 302 status code)
         self.assertEqual(response.status_code, 200)
-        # Check if the user is redirected to the admin dashboard (change this URL as needed)
-        raise Exception(response.read())
-        self.assertRedirects(response.read(), '/admin/')
+        # check that the correct error is returned
+        self.assertContains(response, "Invalid token. Please make sure you have entered it correctly.")
