475989caa8661050333bf17e9f530dc7268de1b4
Rachael Robinson <rachael.robinson@cabinetoffice.gov.uk>
2023-09-20 12:28:32 +0100

    add DBT intranet learning link

--- one_big_thing/learning/constants.py
+++ one_big_thing/learning/constants.py
@@ -32,6 +32,7 @@ MOD_LINK = (
 MOJ_LINK = "https://intranet.justice.gov.uk/guidance/hr/learning-and-development/capability-programmes/one-big-thing/"
 DFE_LINK = "https://educationgovuk.sharepoint.com/sites/lvewp00394/SitePages/One-Big-Thing.aspx"
 DCMS_LINK = "https://intranet.dcms.gov.uk/home/ls/community/my-ld-community"
+DBT_LINK = "https://www.learninghub.trade.gov.uk/course/view.php?id=1178"
 
 
 # These are departments having the "streamlined view"
@@ -70,6 +71,7 @@ ALL_INTRANET_LINKS = OrderedDict(
     {
         "Cabinet Office": CABINET_OFFICE_LINK,
         "Crown Prosecution Service (CPS)": CROWN_PROSECUTION_SERVICE_LINK,
+        "Department for Business and Trade (DBT)": DBT_LINK,
         "Department for Culture, Media and Sport (DCMS)": DCMS_LINK,
         "Department for Education (DfE)": DFE_LINK,
         "Department for Energy Security and Net Zero (DESNZ)": DEPARTMENT_FOR_ENERGY_SECURITY_AND_NET_ZERO_LINK,
