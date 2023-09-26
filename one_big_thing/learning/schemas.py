524046706a4bf5594d09f1928e87fa65bba0afe0
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-09-26 11:03:56 +0100

    update errors for times on recording learning

--- one_big_thing/learning/schemas.py
+++ one_big_thing/learning/schemas.py
@@ -91,13 +91,41 @@ def validate_positive_integer(value, max=None, error_msg="There is an error with
         elif max and (value > max):
             if error_msg_max:
                 raise ValidationError(error_msg_max)
-            raise ValidationError(error_msg)
+            else:
+                raise ValidationError(error_msg)
     except ValueError:
         raise ValidationError(error_msg)
 
 
+def get_error_message_for_integer_validation(value, max=None, error_msg="There is an error with this value", error_msg_max=""):
+    """
+    Checks if value is a positive integer, optionally checks if below max.
+
+    Args:
+        value: Any value to be validated
+        max: Optional value to check value is below max
+        error_msg (str): General error message to display
+        error_msg_max (str): Optional error message if number exceeds max, otherwise error_msg is displayed
+    """
+    if not value:
+        return None
+    try:
+        value = int(value)
+        if value < 0:
+            return error_msg
+        elif max and (value > max):
+            if error_msg_max:
+                return error_msg_max
+            return error_msg
+    except ValueError:
+        return error_msg
+
+
+
 def validate_time_to_complete(value):
-    validate_positive_integer(value, error_msg="Please enter the time this course took to complete in minutes, e.g. 15")
+    error_message = get_error_message_for_integer_validation(value, error_msg="Please enter the time this course took to complete in minutes, e.g. 15")
+    if error_message:
+        raise ValidationError(error_message)
 
 
 def validate_time_to_complete_hours(value):
@@ -133,16 +161,29 @@ class RecordLearningSchema(Schema):
     title = LearningTitleSingleLineStr(required=True)
     link = SingleLineStr(validate=validate.Length(max=256), allow_none=True)
     learning_type = make_choice_field(max_len=256, values=choices.CourseType.values, allow_none=True)
-    time_to_complete_hours = fields.Str(required=False, validate=validate_time_to_complete_hours)
-    time_to_complete_minutes = fields.Str(required=False, validate=validate_time_to_complete_minutes)
+    # Validation for time fields in function below as complex
+    time_to_complete_hours = fields.Str(required=False)
+    time_to_complete_minutes = fields.Str(required=False)
     rating = fields.Str(required=False, allow_none=True)
 
     @validates_schema(skip_on_field_errors=False)
-    def validate_fields(self, data, **kwargs):
+    def validate_time_fields(self, data, **kwargs):
         hours_value = data.get("time_to_complete_hours")
         minutes_value = data.get("time_to_complete_minutes")
+        max_hours_error = f"The course should be less than {constants.HOURS_LIMIT} hours"
+        errors_dictionary = {}
 
-        if not hours_value and not minutes_value:
+        hours_error_msg = get_error_message_for_integer_validation(hours_value, max=constants.HOURS_LIMIT, error_msg=HOURS_ERROR, error_msg_max=max_hours_error)
+        minutes_error_msg = get_error_message_for_integer_validation(minutes_value, error_msg=MINUTES_ERROR)
+
+        if hours_error_msg:
+            errors_dictionary["time_to_complete_hours"] = hours_error_msg
+        if minutes_error_msg:
+            errors_dictionary["time_to_complete_minutes"] = minutes_error_msg
+        if errors_dictionary:
+            raise ValidationError(errors_dictionary)
+
+        if (not hours_value) and (not minutes_value):
             raise ValidationError({"time_to_complete_hours": HOURS_ERROR, "time_to_complete_minutes": MINUTES_ERROR})
 
 
