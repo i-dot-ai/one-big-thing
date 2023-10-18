a9750e3f420c5cd2a3d1f4ee5358197fb14c8bc7
george <g.e.c.burton@gmal.com>
2023-10-18 10:03:04 +0100

    added department to Admin

--- one_big_thing/learning/admin.py
+++ one_big_thing/learning/admin.py
@@ -4,9 +4,14 @@ from django_otp.admin import OTPAdminSite
 from django_otp.plugins.otp_totp.admin import TOTPDeviceAdmin
 from django_otp.plugins.otp_totp.models import TOTPDevice
 
-from one_big_thing.learning.models import User
+from one_big_thing.learning.models import Department, User
 
 from . import models
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ["display", "parent", "url"]
+    list_filter = ["parent"]
 
 
 class SurveyResultAdmin(admin.ModelAdmin):
@@ -21,13 +26,16 @@ class OTPAdmin(OTPAdminSite):
     pass
 
 
-admin_site = OTPAdmin(name="OTPAdmin")
-admin_site.register(User, UserAdmin)
-admin_site.register(TOTPDevice, TOTPDeviceAdmin)
-
 if settings.DEBUG:
     # we only want to expose a minimum of User data
     # outside of DEBUG mode
+    admin_site = admin.AdminSite()
     admin_site.register(models.Course)
     admin_site.register(models.Learning)
     admin_site.register(models.SurveyResult, SurveyResultAdmin)
+    admin_site.register(Department, DepartmentAdmin)
+else:
+    admin_site = OTPAdmin(name="OTPAdmin")
+    admin_site.register(User, UserAdmin)
+    admin_site.register(Department, DepartmentAdmin)
+    admin_site.register(TOTPDevice, TOTPDeviceAdmin)
