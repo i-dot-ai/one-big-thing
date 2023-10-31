d327c42f761042e141ab934bb9fa7c629882b17d
George Burton <8233643+gecBurton@users.noreply.github.com>
2023-10-31 13:23:32 +0000

    Update one_big_thing/learning/management/commands/users_with_seven_hours_no_survey.py

--- one_big_thing/learning/management/commands/users_with_seven_hours_no_survey.py
+++ one_big_thing/learning/management/commands/users_with_seven_hours_no_survey.py
@@ -4,7 +4,7 @@ from one_big_thing.learning.models import User
 
 
 class Command(BaseCommand):
-    help = "users have recorded no learning"
+    help = "users have recorded seven hours but not the end survey"
 
     def handle(self, *args, **kwargs):
         for user in User.objects.seven_hours_no_end_evaluation().values_list("email"):
