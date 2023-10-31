454ad468f1a433f53cea232804175b9f0854cdb4
george <g.e.c.burton@gmal.com>
2023-10-31 15:02:15 +0000

    fixed management commands

--- one_big_thing/learning/management/commands/users_with_seven_hours_no_survey.py
+++ one_big_thing/learning/management/commands/users_with_seven_hours_no_survey.py
@@ -7,5 +7,5 @@ class Command(BaseCommand):
     help = "users have recorded seven hours but not the end survey"
 
     def handle(self, *args, **kwargs):
-        for user in User.objects.seven_hours_no_end_evaluation().values_list("email"):
+        for user in User.objects.seven_hours_no_end_evaluation():
             self.stdout.write(user.email)
