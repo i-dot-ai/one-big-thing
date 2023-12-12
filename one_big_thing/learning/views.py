be6c12b00ed5ab6b6e691ef8785021f69857fef1
Samuel Hoskin <66562886+Samuel-Hoskin@users.noreply.github.com>
2023-12-12 22:02:01 +0000

    Copy If statement in to the record view

--- one_big_thing/learning/views.py
+++ one_big_thing/learning/views.py
@@ -183,6 +183,7 @@ class RecordLearningView(utils.MethodDispatcher):
             template_name = "record-learning.html"
         time_completed = user.get_time_completed()
         completed_obt = time_completed >= settings.REQUIRED_LEARNING_TIME
+        completed_feedback_survey = user.has_completed_post_survey
         learning_types = choices.CourseType.choices
         courses = models.Learning.objects.filter(user=user)
         data = {
@@ -191,6 +192,7 @@ class RecordLearningView(utils.MethodDispatcher):
             "learning_types": learning_types,
             "courses": courses,
             "completed_obt": completed_obt,
+            "completed_feedback_survey": completed_feedback_survey,
         }
         if course_id:
             course = models.Course.objects.filter(pk=course_id).first()
